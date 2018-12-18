a0e8cac4c304366fac180fd52cd93112b9c2f1d6
ding <986746411@qq.com>
2018-12-18 15:42:06 +0800

    无序列表

--- src/core/toolBar/index.ts
+++ src/core/toolBar/index.ts
@@ -3,6 +3,7 @@ import createUndo from './undo';
 import createRedo from './redo';
 import createTitle from './title';
 import createOrderedList from './orderedList';
+import createUnorderedList from './unorderedList'
 
 
 export default function addUserOperation(options: any, editor: WspEditor) {
@@ -10,5 +11,5 @@ export default function addUserOperation(options: any, editor: WspEditor) {
     createRedo(options, editor);
     createTitle(options,editor);
     createOrderedList(options, editor);
-
+    createUnorderedList(options, editor);
 }
